d60bc682636f4fcd2a4b080bb2d2109df6279601
DILAN RODRIGUEZ <diilan.lhsg@gmail.com>
2021-11-30 01:09:02 -0600

    modificamos la ruta

--- js/app.js
+++ js/app.js
@@ -1,15 +1,9 @@
-var url = window.location.href;
-var swLocation = '/DMSERVICE/sw.js';
+
 
 
 
 if ('serviceWorker' in navigator){
-
-      if(url.includes('localhost')){
-           swLocation = '/sw.js';
-      }
-
-     navigator.serviceWorker.register(swLocation);
+     navigator.serviceWorker.register('/sw.js');
 
 }
 
